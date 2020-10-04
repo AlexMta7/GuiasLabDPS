b622ba5c7ca577df0d9911d0373fdea7b4eefbc6
Alex <alexrobertomata@hotmail.com>
2020-10-04 08:01:51 -0600

    Cambios minimos en algún archivo

--- Parcial 2/Clinica/src/app/guard/auth/auth.guard.ts
+++ Parcial 2/Clinica/src/app/guard/auth/auth.guard.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
+//Service
+import { AuthService } from '../../services/auth/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanDeactivate<unknown> {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ){ }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.authService.isLoggedIn !== true) {
+        this.router.navigate(['sign-in'])
+      }
     return true;
   }
   canDeactivate(
