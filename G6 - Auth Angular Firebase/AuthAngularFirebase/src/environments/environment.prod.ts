a650949a7b4c726610604e8bd8206023420817d2
Alex <alexrobertomata@hotmail.com>
2020-11-27 18:27:12 -0600

    Agregando todas las carpertas que se crearon a lo largo del ciclo son todas las guías realizadas

---
+++ G6 - Auth Angular Firebase/AuthAngularFirebase/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: { 
+    apiKey: "AIzaSyB_IpUE2uskg4r0NJWXfZuoVl1bfT-tJTk",
+    authDomain: "authangularfirebase-2141e.firebaseapp.com",
+    databaseURL: "https://authangularfirebase-2141e.firebaseio.com",
+    projectId: "authangularfirebase-2141e",
+    storageBucket: "authangularfirebase-2141e.appspot.com",
+    messagingSenderId: "690590228902",
+    appId: "1:690590228902:web:83fdac566ffa313525af67"
+  }
+};
---
+++ G6 - Auth Angular Firebase/AuthAngularFirebase/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB_IpUE2uskg4r0NJWXfZuoVl1bfT-tJTk",
+    authDomain: "authangularfirebase-2141e.firebaseapp.com",
+    databaseURL: "https://authangularfirebase-2141e.firebaseio.com",
+    projectId: "authangularfirebase-2141e",
+    storageBucket: "authangularfirebase-2141e.appspot.com",
+    messagingSenderId: "690590228902",
+    appId: "1:690590228902:web:83fdac566ffa313525af67"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
